42f6f89c732d48854e63842a1018df466daeb9e7
Sherratt <Sherratt285@gmail.com>
2018-03-12 21:08:14 +0000

    Add static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'static_pages/Item'
+
+  get 'static_pages/FillRecipt'
+
+  get 'static_pages/Reports'
+
+  get 'static_pages/Settings'
+
   root 'application#hello'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
